aaadd6e87a88406b5d30488609b5c7dbd650770b
janma <jan.magnusson@unibw.de>
2023-05-10 12:09:59 -0400

    policy is now evaluated by priority adding a new sub_policy to a text file and into the dictionary of the related policy is now possible (not tested yet) added new policy of low priority to test double check in master_thesis_mqtt project

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ database = mongo_client[db_name]
 collection = database["devices"]
 
 # Add new policies here
-policies = ['thermostat', 'washer', 'washing_machine', 'window']
-policy_methods = {}
+device_types = ['thermostat', 'washer', 'washing_machine', 'window']
+policies = {}
 
 # Dynamically import the policies
-for policy in policies:
-    module = importlib.import_module(f'policies.{policy}')
-    method = getattr(module, f'eval_{policy}_policies')
-    policy_methods[policy] = method
+for device_type in device_types:
+    module = importlib.import_module(f'policies.{device_type}')
+    device_type_policy = getattr(module, f'eval_{device_type}_policies')
+    policies[device_type] = device_type_policy
 
 app = Flask(__name__)
 
@@ -29,14 +29,43 @@ app = Flask(__name__)
 def policy_result(device_type):
     data = request.get_json()
     # Lookup table for policies n(1)
-    method = policy_methods.get(device_type)
-    if method:
-        result = method(data, collection)
-        return jsonify({"result": result})
+    policy_of_requesting_device = policies.get(device_type)
+    if policy_of_requesting_device:
+        result = policy_of_requesting_device(data, collection)
+        return jsonify({"result": result[0], "priority": result[1]})
     else:
         # There is no policy for this device type
         return jsonify({"result": True})
         # return 'Invalid policy name', 400
+
+
+@app.route('/add_policy', methods=['POST'])
+def add_policy():
+    data = request.get_json()
+    priority = data['priority']
+    sub_policy = data['sub_policy']
+    policy_code = data['policy_code']
+    device_type = data['device_type']
+
+    # Dynamically define the method
+    # exec(policy_code)
+
+    # Get the method from the global scope
+    # method = globals()[sub_policy]
+
+    # Dynamically import the specified eval method
+    module = importlib.import_module(f'policies.{device_type}')
+    device_type_policy = getattr(module, f'eval_{device_type}_policies')
+
+    # Add the sub_policy to the list of sub_policies for the specified priority
+    device_type_policy.sub_policies[priority].append(sub_policy)
+
+    # TODO Execute the file in the policy or add the sub_policy directly into the policy
+    # Save the method code to a file
+    with open('methods.txt', 'a') as f:
+        f.write(policy_code + '\n')
+
+    return 'Success'
 
 
 if __name__ == "__main__":
--- policies/washing_machine.py
+++ policies/washing_machine.py
@@ -1,5 +1,29 @@
 def eval_washing_machine_policies(requesting_device, collection):
-    # def eval_policy_solar_panel():
+    sub_policies = {
+        'high_priority': [eval_policy_solar_panel],
+        'medium_priority': [],
+        'low_priority': [eval_policy_machine_unclean]
+    }
+
+    # Execute high priority sub_policies
+    for policy in sub_policies['high_priority']:
+        if not policy(requesting_device, collection):
+            return [False, "high_priority"]
+
+    # Execute medium priority sub_policies
+    for policy in sub_policies['medium_priority']:
+        if not policy(requesting_device, collection):
+            return [False, "medium_priority"]
+
+    # Execute low priority sub_policies
+    for policy in sub_policies['low_priority']:
+        if not policy(requesting_device, collection):
+            return [False, "low_priority"]
+
+    return [True, "N/A"]
+
+
+def eval_policy_solar_panel(requesting_device, collection):
     # Retrieve solar panel data from database
     solar_panel = collection.find_one({"device_id": "solar_panel"})
 
@@ -10,3 +34,7 @@ def eval_washing_machine_policies(requesting_device, collection):
 
     return solar_panel["provided_power"] >= total_power + requesting_device["work_power"]
 
+
+def eval_policy_machine_unclean(requesting_device, collection):
+    print(requesting_device)
+    return requesting_device["last_cleaning"] < 4
